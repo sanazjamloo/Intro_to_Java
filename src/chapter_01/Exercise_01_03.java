a24d306d564fde1488d53d6aeb8c8103d467631b
sanazjamloo <sanaz.jamloo@gmail.com>
2018-04-23 12:14:15 -0700

    Edit exercise_01_03

--- src/chapter_01/Exercise_01_03.java
+++ src/chapter_01/Exercise_01_03.java
@@ -1,5 +1,7 @@
 package chapter_01;
 
+/* Write a program that prints the below pattern*/
+
 public class Exercise_01_03 {
 
 	public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class Exercise_01_03 {
 		System.out.println("    J     A     V     V     A");
 		System.out.println("    J    A A     V   V     A A");
 		System.out.println("J   J   AAAAA     V V     AAAAA");
-		System.out.println(" J  J  A     A      V    A     A");
+		System.out.println(" J  J  A     A     V     A     A");
 		
 	}
 
