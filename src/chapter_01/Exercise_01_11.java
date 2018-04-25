73bc5c5645de13c55bca1136b4687cfd7a3b3eae
sanazjamloo <sanaz.jamloo@gmail.com>
2018-04-24 19:10:17 -0700

    Edit exercise_01_11

--- src/chapter_01/Exercise_01_11.java
+++ src/chapter_01/Exercise_01_11.java
@@ -15,26 +15,26 @@ public class Exercise_01_11 {
 	public static void main(String[] args) {
 		
 		// defining variables or storing each value in a variable so we can call them later on.
-				int birth = 7;
-				int death = 13;
-				int immigrant = 45;
-				int currentPopulation = 312032486;
-				int year = 365;
-				
-				//calculating population in one year
-				int BirthInOneYear = (((year * 24 * 60 * 60) / birth));
-				int DeathInOneYear = (((year * 24 * 60 * 60) / death));
-				int ImmigrantInOneYear = (((year * 24 * 60 * 60) / immigrant));
-				int PopulationInOneYear = (BirthInOneYear + ImmigrantInOneYear - DeathInOneYear);
-				
-				//printing the population for the next 5 years based on 
-				System.out.println("This Year's Population is: " + currentPopulation);
-				System.out.println("Population of one year will be: " + (PopulationInOneYear));
-				System.out.println("Next year's population will be: " + (currentPopulation + PopulationInOneYear));
-				System.out.println("Pouplation in 2 years will be: " + (currentPopulation + (PopulationInOneYear * 2)));
-				System.out.println("Pouplation in 3 years will be: " + (currentPopulation + (PopulationInOneYear * 3)));
-				System.out.println("Pouplation in 4 years will be: " + (currentPopulation + (PopulationInOneYear * 4)));
-				System.out.println("Pouplation in 5 years will be: " + (currentPopulation + (PopulationInOneYear * 5)));
+		int birth = 7;
+		int death = 13;
+		int immigrant = 45;
+		int currentPopulation = 312032486;
+		int year = 365;
+		
+		//calculating population in one year
+		int BirthInOneYear = (((year * 24 * 60 * 60) / birth));
+		int DeathInOneYear = (((year * 24 * 60 * 60) / death));
+		int ImmigrantInOneYear = (((year * 24 * 60 * 60) / immigrant));
+		int PopulationInOneYear = (BirthInOneYear + ImmigrantInOneYear - DeathInOneYear);
+		
+		//printing the population for the next 5 years based on 
+		System.out.println("This Year's Population is: " + currentPopulation);
+		System.out.println("Population of one year will be: " + (PopulationInOneYear));
+		System.out.println("Next year's population will be: " + (currentPopulation + PopulationInOneYear));
+		System.out.println("Pouplation in 2 years will be: " + (currentPopulation + (PopulationInOneYear * 2)));
+		System.out.println("Pouplation in 3 years will be: " + (currentPopulation + (PopulationInOneYear * 3)));
+		System.out.println("Pouplation in 4 years will be: " + (currentPopulation + (PopulationInOneYear * 4)));
+		System.out.println("Pouplation in 5 years will be: " + (currentPopulation + (PopulationInOneYear * 5)));
 
 
 	}
