5e089dfcc00081d388e7802caa5da39d47fdd05f
Sanaz Jamloo <sanaz@Sanazs-MBP.attlocal.net>
2018-05-22 21:04:34 -0700

    Added chapter_07

---
+++ src/chapter_07/Exercise_07_12.java
@@ -0,0 +1,43 @@
+package chapter_07;
+/*The reverse method in exercise 07_07 reverses an array by copying it to a new array. 
+ *Rewrite the method that reverses the array passed in the argument and returns this array.  
+ *Write a test program that prompts the user to enter ten numbers, invokes this method
+ * to reverse the numbers, and displays the numbers.
+ */
+
+import java.util.Scanner;
+
+
+public class Exercise_07_12 {
+
+	/** Main method */
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);	// Create a Scanner
+		int[] numbers = new int[10];	// Create an array of length ten
+
+		// Prompt the user to enter ten numbers
+		System.out.print("Enter ten numbers: ");
+		for (int i = 0; i < numbers.length; i++)
+			numbers[i] = input.nextInt();
+
+		// Invoke the method to reverse the numbers
+		reverse(numbers);
+
+		// Display the numbers
+		for (int e: numbers) {
+			System.out.print(e + " ");
+		}
+		System.out.println();
+	}
+
+	/** Method reverse reverses the array passed in the argument */
+	public static void reverse(int[] list) {
+		int temp;
+		for (int i = 0, j = list.length - 1; i < list.length / 2; i++, j--) {
+			temp = list[i];
+			list[i] = list[j];
+			list[j] = temp;
+		}
+	}
+
+}
