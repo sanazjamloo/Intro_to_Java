ff159a3255c18bd0d75143a9f69ec568213d8c45
Sanaz Jamloo <sanaz@Sanazs-MBP.attlocal.net>
2018-08-01 19:05:35 -0700

    Add exrecise 01_08

---
+++ src/chapter_01/Exercise_01_08.java
@@ -0,0 +1,13 @@
+package chapter_01;
+/* Write a program that displays the area and perimeter of
+ * a circle that has a radius of 5.5. 
+ */
+
+public class Exercise_01_08 {
+
+	public static void main(String[] args) {
+		System.out.println("Perimeter is: " + 2 *(5.5 * 3.14));
+		System.out.println("Area is: " + (5.5 * 5.5 * 3.14));
+	}
+
+}
