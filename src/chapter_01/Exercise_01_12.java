6f329fac49e6d5352ca711e05713cd029fd55df6
Sanaz Jamloo <sanaz@Sanazs-MBP.attlocal.net>
2018-08-27 21:23:47 -0700

    Add exercise_02_01

---
+++ src/chapter_01/Exercise_01_12.java
@@ -0,0 +1,15 @@
+package chapter_01;
+/*Assume a runner runs 24 miles in 1 hour, 40 minutes, and 35 seconds.
+ * Write a program that displays the average speed in kilometers per hour.
+ * Note that 1 mile is 1.6 kilometers.
+ * 
+ */
+
+public class Exercise_01_12 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
